f6ff4fe999f85eb3336a8108b18868aa4b96b0f5
GardenLu <lhypersonmail@163.com>
2021-10-16 16:47:44 +0800

    automatic push @2021年 10月 16日 星期六 16:47:44 CST

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-path=~/github/
+echo ${1}
 
 git_push(){
 	echo "开始push"
@@ -11,21 +10,24 @@ git_push(){
 	echo "-------切换目录------"
 	echo `pwd`
 	echo "---------------------"
-	if [ $[ ${modify_time}-${this_time} ] -gt 86400 ];
-	then
-		echo "${1} 文件夹 有变化，正在准备push..."
-		date=`date "+%Y-%m-%d %H:%M:%S"`
-		git add .    
-		git commit -m "automatic push @$(date)"
-		echo "git fetch origin master"
-		git fetch origin master
+#	echo  [$[ ${modify_time}-${this_time} ] -gt 86400 ] 
+#	if [ $[ ${modify_time}-${this_time} ] -gt 86400 ];
+#	then
+	echo "${1} 文件夹 有变化，正在准备push..."
+	date=`date "+%Y-%m-%d %H:%M:%S"`
+	git add .    
+	git commit -m "automatic push @$(date)"
+	echo "git fetch origin master"
+	git fetch origin master
 
-		echo "git merge origin/master"
-		git merge origin/master
+	echo "git merge origin/master"
+	git merge origin/master
 
-		echo "git push origin master:master"
-		git push origin master:master
+	echo "git push origin master:master"
+	git push origin master:master
 
-	fi
+#	fi
 }
+
+git_push /media/18tdisk/sharedata/codes/offer
 
