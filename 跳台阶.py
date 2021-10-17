58e7bcb2f589be4804110c595853e67591be1f9f
GardenLu <lhypersonmail@163.com>
2021-10-17 14:18:01 +0800

    automatic push @2021年 10月 17日 星期日 14:18:01 CST

--- 跳台阶.py
+++ 跳台阶.py
@@ -1,21 +1,21 @@
-
 """
 Author: your name
-Date: 2021-10-17 14:12:49
-LastEditTime: 2021-10-17 14:12:49
-LastEditors: Please set LastEditors
+Date: 2021-10-17 14:15:36
+LastEditTime: 2021-10-17 14:15:37
+LastEditors: your name
 Description: In User Settings Edit
 FilePath: /offer/跳台阶.py
 """
-
 
 # https://www.nowcoder.com/practice/8c82a5b80378478f9484d87d1c5f12a4?tpId=188&rp=1&ru=%2Factivity%2Foj&qru=%2Fta%2Fjob-code-high-week%2Fquestion-ranking
 
 # -*- coding:utf-8 -*-
 class Solution:
+    
     def jumpFloor(self, number):
         # write code here
-        n
+        steps = []
+        
 
 
 if __name__ == "__main__":
