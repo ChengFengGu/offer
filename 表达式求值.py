b07f938d8b3b869c64a28a70603930d949c74256
GardenLu <lhypersonmail@163.com>
2021-10-17 16:45:01 +0800

    automatic push @2021年 10月 17日 星期日 16:45:01 CST

--- 表达式求值.py
+++ 表达式求值.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-10-17 16:36:00
-LastEditTime: 2021-10-17 16:41:21
+LastEditTime: 2021-10-17 16:44:49
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /offer/表达式求值.py
@@ -12,4 +12,11 @@ class Solution:
         # 符号栈 数字栈 计算优先级 括号>乘>加=减
         # 符号栈： 规则： 遇到一半括号，则等待另一个括号再进行计算
         # 数字符号全入栈，遇加减等待，遇乘法等待，遇一半括号，则等待另一个括号再进行计算
+        ops = []
+        nums = []
         
+        for char in s:
+            if char in ["+","-","*","(",")"]:
+                ops.append(char)
+            if char in ['{i}']
+            
