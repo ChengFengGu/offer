6de1945c23625fa0a70e37aa06eaaf0450d8d789
GardenLu <lhypersonmail@163.com>
2021-10-17 23:21:01 +0800

    automatic push @2021年 10月 17日 星期日 23:21:01 CST

--- 生日聚会.py
+++ 生日聚会.py
@@ -1,12 +1,15 @@
 '''
 Author: your name
-Date: 2021-10-17 22:52:19
-LastEditTime: 2021-10-17 23:17:49
-LastEditors: Please set LastEditors
+Date: 2021-10-17 23:20:47
+LastEditTime: 2021-10-17 23:20:47
+LastEditors: your name
 Description: In User Settings Edit
 FilePath: /offer/生日聚会.py
 '''
 
 
 if __name__ == "__main__":
+    N = int(input())
+    data = eval(input())
     
+    for 
