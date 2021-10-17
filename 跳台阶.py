937b4193014b066a105178bc99337aa4433d9dd0
GardenLu <lhypersonmail@163.com>
2021-10-17 15:03:01 +0800

    automatic push @2021年 10月 17日 星期日 15:03:01 CST

--- 跳台阶.py
+++ 跳台阶.py
@@ -30,11 +30,12 @@ class Solution:
         dp[1] = 1
         dp[2] = 2
         
+        sum = 0
         for i in range(3,number):
             dp[i] = dp[i-1] + dp[i-2]
-        return dp[-1]
+        return 
 
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.jumpFloor(35))
+    print(sol.jumpFloor_infer(35))
