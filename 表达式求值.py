c241b04c76932aef3b33924143e2f5794b540fe9
GardenLu <lhypersonmail@163.com>
2021-10-17 17:24:02 +0800

    automatic push @2021年 10月 17日 星期日 17:24:02 CST

--- 表达式求值.py
+++ 表达式求值.py
@@ -9,20 +9,33 @@ FilePath: /offer/表达式求值.py
 
 
 class Solution:
+    def __init__(self):
+        ops_prio = {"+":1, "-":1, "*":2, "(":0, ")":3}
+    
+    def mid2after(self,s):
+        for char in s:
+            if char in list(self.ops_prio.keys()):
+                prio = self.ops_prio.get(char)
+                top = self.ops_prio.get(ops[-1])
+                if top!=None and self.ops_prio.get(top)>=prio:
+                    pass
+
     def solve(self, s):
         # write code here
         # 符号栈 数字栈 计算优先级 括号>乘>加=减
         # 符号栈： 规则： 遇到一半括号，则等待另一个括号再进行计算
         # 数字符号全入栈，遇加减等待，遇乘法等待，遇一半括号，则等待另一个括号再进行计算
-        ops = []
+        self.
         nums = []
         ops_prio = {"+":1, "-":1, "*":2, "(":0, ")":3}
         s = s.replace(" ",'')
+        s_after = ""
         for char in s:
             if char in list(ops_prio.keys()):
                 prio = ops_prio.get(char)
-                top = ops.pop()
-                if top!=None and ops_prio.get(top)<=
+                top = ops_prio.get(ops[-1])
+                if top!=None and ops_prio.get(top)>=prio:
+                    pass
                     
             if char in ["{i}" for i in range(0, 9)]:
                 nums.append(char)
