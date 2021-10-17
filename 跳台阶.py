7fbb52d65c0971ad64703e54a01555d74c7a0cf0
GardenLu <lhypersonmail@163.com>
2021-10-17 14:24:01 +0800

    automatic push @2021年 10月 17日 星期日 14:24:01 CST

--- 跳台阶.py
+++ 跳台阶.py
@@ -10,14 +10,19 @@ FilePath: /offer/跳台阶.py
 # https://www.nowcoder.com/practice/8c82a5b80378478f9484d87d1c5f12a4?tpId=188&rp=1&ru=%2Factivity%2Foj&qru=%2Fta%2Fjob-code-high-week%2Fquestion-ranking
 
 # -*- coding:utf-8 -*-
+
+# https://www.nowcoder.com/practice/8c82a5b80378478f9484d87d1c5f12a4?tpId=188&rp=1&ru=%2Factivity%2Foj&qru=%2Fta%2Fjob-code-high-week%2Fquestion-ranking
 class Solution:
-    
     def jumpFloor(self, number):
         # write code here
-        steps = []
-        
+        if number <= 0:
+            return -1
+        elif number == 1 or number == 2:
+            return number
+        else:
+            return self.jumpFloor(number - 1) + self.jumpFloor(number - 2)
 
 
 if __name__ == "__main__":
     sol = Solution()
-    sol.jumpFloor(2)
+    print(sol.jumpFloor(7))
