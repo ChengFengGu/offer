383ba15ec0ad1f40f42bd91ded17a914b7ea475b
GardenLu <lhypersonmail@163.com>
2021-10-17 23:24:01 +0800

    automatic push @2021年 10月 17日 星期日 23:24:01 CST

--- 生日聚会.py
+++ 生日聚会.py
@@ -1,8 +1,8 @@
 '''
 Author: your name
 Date: 2021-10-17 23:20:47
-LastEditTime: 2021-10-17 23:20:47
-LastEditors: your name
+LastEditTime: 2021-10-17 23:23:00
+LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /offer/生日聚会.py
 '''
@@ -12,4 +12,9 @@ if __name__ == "__main__":
     N = int(input())
     data = eval(input())
     
+    num = 1
+    
+    matrix = [[0 for _ in range(N)] for i in range(N)]
+    
     for 
+    
