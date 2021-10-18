b4b4206454395a052626aeb2099cbd3ec4665263
GardenLu <lhypersonmail@163.com>
2021-10-18 10:15:01 +0800

    automatic push @2021年 10月 18日 星期一 10:15:01 CST

--- 所有子集.py
+++ 所有子集.py
@@ -4,8 +4,8 @@
 def helper(nums: list, index: int, subset: list, result: list):
     if index == len(nums):
         result.append(subset)
-    elif index < len(nums):
+    elif index < len(nums): #有两个选择 是否将本字符串加入集合，分别选择加入和非加入，然后bian'li'ch
         helper(nums, index + 1, subset, result)
         subset.append(nums[index])
-
         helper(nums, index + 1, subset, result)
+        subset.pop()
