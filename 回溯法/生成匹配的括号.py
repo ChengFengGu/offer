bc284655bb34b14391cd03b4e66c300f094239cc
GardenLu <lhypersonmail@163.com>
2021-10-18 12:27:01 +0800

    automatic push @2021年 10月 18日 星期一 12:27:01 CST

--- 回溯法/生成匹配的括号.py
+++ 回溯法/生成匹配的括号.py
@@ -1,5 +1,13 @@
 def helper(n,index,container,result):
-    if index == n:
+    if index == 2*n:
+        left_num = 0
+        right_num = 0
+        for e in container:
+            if e == "(": left_num += 1
+            if e == ")": right_num += 1
+        if left_num == right_num:
+            result.append(container)
+    else:
         
 
 
