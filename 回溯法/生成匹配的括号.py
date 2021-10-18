9aa2886d52dd01f398d86b37cea8509c747c37d6
GardenLu <lhypersonmail@163.com>
2021-10-18 12:30:01 +0800

    automatic push @2021年 10月 18日 星期一 12:30:01 CST

--- 回溯法/生成匹配的括号.py
+++ 回溯法/生成匹配的括号.py
@@ -1,15 +1,25 @@
-def helper(n,index,container,result):
-    if index == 2*n:
+def helper(n, index, container, result):
+    if index == 2 * n:
         left_num = 0
         right_num = 0
         for e in container:
-            if e == "(": left_num += 1
-            if e == ")": right_num += 1
+            if e == "(":
+                left_num += 1
+            if e == ")":
+                right_num += 1
         if left_num == right_num:
-            result.append(container)
+            result.append(container.copy())
     else:
-        
+        container.append("(")
+        helper(n, index + 1, container, result)
+        container.pop()
+
+        container.append(")")
+        helper(n, index + 1, container, result)
+        container.pop()
 
 
-def generate(n:int):
-    pass
+def generate(n: int):
+    container = []
+    result = []
+    helper = helper(n, index)
