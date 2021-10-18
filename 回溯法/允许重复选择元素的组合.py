8947f3f80008641c4cb6c619af4ae8ed01de2eac
GardenLu <lhypersonmail@163.com>
2021-10-18 10:42:01 +0800

    automatic push @2021年 10月 18日 星期一 10:42:01 CST

--- 回溯法/允许重复选择元素的组合.py
+++ 回溯法/允许重复选择元素的组合.py
@@ -1,12 +1,19 @@
-
-
-
 def helper(nums: list, k: int, index: int, subset: list, result: list):
     if sum(subset) == k:
         result.append(subset.copy())
     elif index < len(nums):
+
+        helper(nums, k, index + 1, subset, result)
         subset.append(nums[index])
+        helper(nums, k, index + 1, subset, result)
+        subset.pop()
 
 
 def subsets(nums: list, k: int):
-    pass
+    subset = []
+    result = []
+    helper(nums, k, 0, subset, result)
+    return result
+
+if __name__ == '__main__':
+    print(subsets([2,3,5],8))
