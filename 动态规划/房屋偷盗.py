c142c54c36bb4f9155654c0d00c5c7acf216a881
GardenLu <lhypersonmail@163.com>
2021-10-18 15:36:01 +0800

    automatic push @2021年 10月 18日 星期一 15:36:01 CST

--- 动态规划/房屋偷盗.py
+++ 动态规划/房屋偷盗.py
@@ -67,5 +67,11 @@ def robv4(nums: list):
     return max(dp[0][(length - 1) % 2], dp[1][(length - 1) % 2])
 
 
+
+# 环形房屋偷盗，分解为两个问题，max((0-n-2) ; 1-(n-1))
+
+
+
+
 if __name__ == "__main__":
     print(robv4([1, 2, 3, 4, 5]))
