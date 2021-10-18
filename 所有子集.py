7543b04cb866353e970666cd79cf86b48777b217
GardenLu <lhypersonmail@163.com>
2021-10-18 10:03:01 +0800

    automatic push @2021年 10月 18日 星期一 10:03:01 CST

--- 所有子集.py
+++ 所有子集.py
@@ -1,10 +1,12 @@
 '''
 Author: your name
 Date: 2021-10-18 09:59:38
-LastEditTime: 2021-10-18 09:59:39
+LastEditTime: 2021-10-18 10:02:09
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /offer/所有子集.py
 '''
 
-def 
+def all_subgroups(nums:list,index:int):
+    if index == len(nums):
+        
