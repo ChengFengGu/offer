93467b351e256e8ee2994c87556f83599dbc9ebb
GardenLu <lhypersonmail@163.com>
2021-10-18 11:21:01 +0800

    automatic push @2021年 10月 18日 星期一 11:21:01 CST

--- 回溯法/全排列.py
+++ 回溯法/全排列.py
@@ -1,16 +1,17 @@
 # 题目：给定一个没有重复数字的集合，请找出它的所有全排列。例如，集合[1，2，3]有6个全排列，分别是[1，2，3]、[1，3，2]、[2，1，3]、[2，3，1]、[3，1，2]和[3，2，1]
 
 
-def helper(nums: int, index: int, set: list, result: list):
+def helper(nums: int, index: int, result: list):
 
-    if len(set) == len(nums):
+    if index == len(nums):
         permutation = []
         for num in nums:
             permutation.append(num)
         result.append(permutation.copy())
     else:
         for j in range(len(nums)):
-            swap(nums)
+            swap(nums, index, j)
+            helper(nums, index + 1, result)
 
 
 def swap(nums: list, i, j):
@@ -21,9 +22,9 @@ def swap(nums: list, i, j):
 
 
 def sets(nums: list):
-    set = []
+
     result = []
-    helper(nums, 0, set, result)
+    helper(nums, 0, result)
     return result
 
 
