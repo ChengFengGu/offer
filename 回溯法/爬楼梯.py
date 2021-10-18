02263d68d8e53bc02847b6bb5b113fa5f249d747
GardenLu <lhypersonmail@163.com>
2021-10-18 13:45:01 +0800

    automatic push @2021年 10月 18日 星期一 13:45:01 CST

--- 回溯法/爬楼梯.py
+++ 回溯法/爬楼梯.py
@@ -2,11 +2,13 @@ def helper(cost: list, index: int):
     if index <= 2:
         return cost[index]
     else:
-        return min(helper(cost, index-1),helper(cost, index-2))
+        return min(helper(cost, index - 1), helper(cost, index - 2)) + cost[index]
+
 
 def mincost(cost: list):
-    return helper(cost,0)
+    result = helper(cost, 0)
+    return result
 
 
-if __name__ == '__main__':
-    print(mincost([1,2,3])
+if __name__ == "__main__":
+    print(mincost([3, 5, 3,1,7]))
