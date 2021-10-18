68b709cf6a15e527d35d607e0581d4dde6ff44f3
GardenLu <lhypersonmail@163.com>
2021-10-18 12:57:01 +0800

    automatic push @2021年 10月 18日 星期一 12:57:01 CST

--- 回溯法/分割回文字符串.py
+++ 回溯法/分割回文字符串.py
@@ -1,23 +1,31 @@
 # 题目：输入一个字符串，要求将它分割成若干子字符串，使每个子字符串都是回文。请列出所有可能的分割方法。例如，输入"google"，将输出3种符合条件的分割方法，分别是["g"，"o"，"o"，"g"，"l"，"e"]、["g"，"oo"，"g"，"l"，"e"]和["goog"，"l"，"e"]。
 
-def judge(string:str):
+
+def judge(string: str):
     mid = len(string) // 2
     flag = True
     for i in range(mid):
-        if string[i] != string[-(i+1)]:
+        if string[i] != string[-(i + 1)]:
             flag = False
     return flag
 
 
-def helper(string:str,index:int,container:str,result:list):
-    
-    if index <= len(string):
-        if judge(container):
-            result.append(container.copy())
+def helper(string: str, index: int, container: str, result: list):
 
-        container
-    pass
+    if judge(container):
+        result.append(container)
+    elif index <= len(string):
+        container.append(string[index])
+        helper(string, index + 1, container, result)
+        container.pop()
 
 
-def generate(string:str):
-    pass
+def generate(string: str):
+    container = ""
+    result = []
+    helper(string, 0, container, result)
+    return result
+
+
+if __name__ == "__main__":
+    print(generate("google"))
