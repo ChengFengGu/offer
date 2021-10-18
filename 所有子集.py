04c8be75deca0a86b4d4810400189849b7dd45e3
GardenLu <lhypersonmail@163.com>
2021-10-18 10:18:01 +0800

    automatic push @2021年 10月 18日 星期一 10:18:01 CST

--- 所有子集.py
+++ 所有子集.py
@@ -1,11 +1,9 @@
-
-
-
 def helper(nums: list, index: int, subset: list, result: list):
     if index == len(nums):
-        result.append(subset)
-    elif index < len(nums): #有两个选择 是否将本字符串加入集合，分别选择加入和非加入，然后bian'li'ch
+        result.append(subset.copy())
+    elif index < len(nums):  # 有两个选择 是否将本字符串加入集合，分别选择加入和非加入，然后递归即可
         helper(nums, index + 1, subset, result)
         subset.append(nums[index])
         helper(nums, index + 1, subset, result)
         subset.pop()
+    
