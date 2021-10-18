ab92066f5a970f985ff9c147d13af022e44d5475
GardenLu <lhypersonmail@163.com>
2021-10-18 10:33:01 +0800

    automatic push @2021年 10月 18日 星期一 10:33:01 CST

--- 回溯法/Cnk.py
+++ 回溯法/Cnk.py
@@ -1,10 +1,13 @@
 def helper(n: int, k: int, index: int, combination: list, result: list):
     if len(combination) == k:
-        result.append(combination)
-    elif i <= n:
-        helper()
+        result.append(combination.copy())
+    elif index <= n:
+        helper(n,k,index+1,combination,result)
+        combination.append(index)
+        helper(n,k,index+1,combination,result)
+        combination.pop()
 
-
-
-def subsets(nums: int):
-    pass
+def subsets(n,k):
+    result = []
+    combination = []
+    helper(n,k,)
