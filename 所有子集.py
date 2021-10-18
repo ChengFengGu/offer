9381ff868d6d8ceca03a1958e5c75f4ed655883b
GardenLu <lhypersonmail@163.com>
2021-10-18 10:06:01 +0800

    automatic push @2021年 10月 18日 星期一 10:06:01 CST

--- 所有子集.py
+++ 所有子集.py
@@ -1,12 +1,24 @@
 '''
 Author: your name
-Date: 2021-10-18 09:59:38
-LastEditTime: 2021-10-18 10:02:09
-LastEditors: Please set LastEditors
+Date: 2021-10-18 10:05:15
+LastEditTime: 2021-10-18 10:05:15
+LastEditors: your name
 Description: In User Settings Edit
 FilePath: /offer/所有子集.py
 '''
+"""
+Author: your name
+Date: 2021-10-18 09:59:38
+LastEditTime: 2021-10-18 10:04:13
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /offer/所有子集.py
+"""
 
-def all_subgroups(nums:list,index:int):
+
+def helper(nums: list, index: int, subset: list, result: list):
     if index == len(nums):
+        result.append(subset)
+    elif index < len(nums):
+        result = helper(nums, index + 1, subset, result)
         
