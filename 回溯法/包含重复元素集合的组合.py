011ea17e59113e699582d153ba4adc31553c89e6
GardenLu <lhypersonmail@163.com>
2021-10-18 11:00:01 +0800

    automatic push @2021年 10月 18日 星期一 11:00:01 CST

--- 回溯法/包含重复元素集合的组合.py
+++ 回溯法/包含重复元素集合的组合.py
@@ -1,10 +1,24 @@
 def helper(nums: list, k: int, index: int, subset: list, result: list):
     if k == 0:
-        result.append(subset.copy())
+        result.append(sorted(subset.copy()))
     elif index < len(nums):
-        helper(nums,k,index+1,subset,result)
-        
+        helper(nums, k, index + 1, subset, result)
+        subset.append(nums[index])
+        helper(nums, k, index + 1, subset, result)
+        subset.pop()
 
 
 def subsets(nums: list, k: int):
-    pass
+
+    subset = []
+    result = []
+
+    helper(nums, k, 0, subset, result)
+
+    return result
+
+    print(result)
+
+
+if __name__ == "__main__":
+    subsets([2, 2, 2, 4, 3, 3], 8)
