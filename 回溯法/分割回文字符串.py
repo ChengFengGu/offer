e7c051338f6649d1d361ebab6324402beb79f183
GardenLu <lhypersonmail@163.com>
2021-10-18 12:51:01 +0800

    automatic push @2021年 10月 18日 星期一 12:51:01 CST

--- 回溯法/分割回文字符串.py
+++ 回溯法/分割回文字符串.py
@@ -1,7 +1,12 @@
 # 题目：输入一个字符串，要求将它分割成若干子字符串，使每个子字符串都是回文。请列出所有可能的分割方法。例如，输入"google"，将输出3种符合条件的分割方法，分别是["g"，"o"，"o"，"g"，"l"，"e"]、["g"，"oo"，"g"，"l"，"e"]和["goog"，"l"，"e"]。
 
 def judge(string:str):
-    mid = int()
+    mid = len(string) // 2
+    flag = True
+    for i in range(mid):
+        if string[i] != string[-(i+1)]:
+            flag = False
+    return flag
 
 
 def helper(string:str,index:int,container:str,result:list):
