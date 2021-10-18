ce2074682707ee103e581da99133b59bc1ea313a
GardenLu <lhypersonmail@163.com>
2021-10-18 10:21:01 +0800

    automatic push @2021年 10月 18日 星期一 10:21:01 CST

--- 所有子集.py
+++ 所有子集.py
@@ -5,5 +5,18 @@ def helper(nums: list, index: int, subset: list, result: list):
         helper(nums, index + 1, subset, result)
         subset.append(nums[index])
         helper(nums, index + 1, subset, result)
-        subset.pop()
-    
+        subset.pop()  # 在回溯到父节点之前，应该清除已经对子集状态进行的修改。此前在子集subset中添加了一个数字，此时应该将它删除。
+
+
+def subsets(nums: list):
+    result = []
+    if len(nums) == 0:
+        return result
+    subset = []
+    result = []
+    helper(nums, 0, subset, result)
+    return result
+
+
+if __name__ == "__main__":
+    print(subsets([1,2,3]))
