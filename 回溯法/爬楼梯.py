a50b3dfb81518134b16d997b01f1a4dd5a4b9805
GardenLu <lhypersonmail@163.com>
2021-10-18 14:30:01 +0800

    automatic push @2021年 10月 18日 星期一 14:30:01 CST

--- 回溯法/爬楼梯.py
+++ 回溯法/爬楼梯.py
@@ -25,7 +25,7 @@ def mincost_v2(cost: list):
     dp[0] = cost[0]
     dp[1] = cost[1]
 
-    for i in range(3, len(cost)):
+    for i in range(2, len(cost)):
         dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
 
     return min(dp[len(cost) - 2], dp[len(cost) - 1])
