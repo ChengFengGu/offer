53e6f2acebadfffd63a7252e2638cde3154dc121
GardenLu <lhypersonmail@163.com>
2021-10-19 14:45:01 +0800

    automatic push @2021年 10月 19日 星期二 14:45:01 CST

--- 推荐算法/prob_codes/二项分布.py
+++ 推荐算法/prob_codes/二项分布.py
@@ -2,3 +2,19 @@ import numpy as np
 from scipy import stats
 
 import matplotlib.pyplot as plt
+
+
+def binom_pmf_test():
+
+    n = 100
+    p = 0.5
+    k = np.arange(0, 100)
+
+    binomal = stats.binom.pmf(k, n, p)
+
+    print(f"概率和为=>{binomal} binomal[2]:{binomal[2]}")
+    plt.plot(k,binomal,'o-')
+    plt.xlabel("Number of successive")
+    plt.ylabel("Number of successive")
+
+
