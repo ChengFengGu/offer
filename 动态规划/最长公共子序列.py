31790a50354ae502d3f354ff9f80602758bfe126
GardenLu <lhypersonmail@163.com>
2021-10-20 21:12:01 +0800

    automatic push @2021年 10月 20日 星期三 21:12:01 CST

--- 动态规划/最长公共子序列.py
+++ 动态规划/最长公共子序列.py
@@ -13,9 +13,17 @@ def longest_sub_array(s1: str, s2: str):
     for i in range(len(s1)):
         for j in range(len(s2)):
             if i == 0 or j == 0:
-                
+                dp[i, j] = 1  # 空字符串作为最长公共子序列
+            else:
+                if s1[i] == s2[j]:
+                    dp[i, j] = dp[i - 1, j - 1] + 1
+                else:
+                    dp[i, j] = max(dp[i - 1, j], dp[i, j - 1])
+    return dp[len(s1) - 1, len(s2) - 1]
 
 
 if __name__ == "__main__":
 
-    pass
+    print(longest_sub_array("abd", "abe"))
+
+# %%
