cdfcae0d0ed1b079869ce43402681a7614cd9b11
GardenLu <lhypersonmail@163.com>
2021-10-21 17:00:01 +0800

    automatic push @2021年 10月 21日 星期四 17:00:01 CST

--- 动态规划/三角形最小路径和.py
+++ 动态规划/三角形最小路径和.py
@@ -10,13 +10,14 @@ class Solution:
         dp = [[math.inf for _ in range(width)] for i in range(len(triangle))]
 
         dp[0][0] = triangle[0][0]
-
+        for i in range(width):
+            dp[i][0] = sum([dp[k][0] for k in range()])
         for i in range(1, len(triangle)):  # j代表层数
             for j in range(0, len(triangle[i])):  #
                 if j == 0:
-                    dp[i][j] = min(dp[i][0], dp[i][1]) + triangle[i][j]
+                    dp[i][j] = dp[i][0] + triangle[i][j]
                 else:
-                    dp[i][j] = min(dp[i - 1][j], dp[i - 1][j + 1]) + triangle[i][j]
+                    dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j]
         return min(dp[len(triangle) - 1])
 
 
