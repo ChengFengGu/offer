2e0ee0325c16fdcf9109ccd2cc24fa91fb2834c1
GardenLu <lhypersonmail@163.com>
2021-10-21 15:42:01 +0800

    automatic push @2021年 10月 21日 星期四 15:42:01 CST

--- 动态规划/子序列数目.py
+++ 动态规划/子序列数目.py
@@ -21,8 +21,8 @@ class Solution:
                     dp[i + 1][j + 1] = dp[i][j + 1]
         return dp[len(s)][len(t)]
 
-    def numDistinctv2(self,s: str, t: str):
-        dp = [[0 for _ in range(len(s))] for _ in range(len(t))]
+    def numDistinctv2(self, s: str, t: str):
+        dp = [[0 for _ in range(len(t) + 1)] for _ in range(len(s) + 1)]
 
         dp[0][0] = 1
 
@@ -33,7 +33,7 @@ class Solution:
                 if s[i] == t[j]:
                     dp[i + 1][j + 1] = dp[i][j] + dp[i][j + 1]
                 else:
-                    dp[i+1][j+1] = dp[i][j+1]
+                    dp[i + 1][j + 1] = dp[i][j + 1]
         return dp[len(s)][len(t)]
 
 
