077f3a0bb9b8b6a27755ed98a3a31c5c18121aad
GardenLu <lhypersonmail@163.com>
2021-10-21 16:12:01 +0800

    automatic push @2021年 10月 21日 星期四 16:12:01 CST

---
+++ 动态规划/最小路径之和.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minPathSum(self, grid: list) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(m):
+            for j in range(n):
+                if i == 0:
+                    dp[i][j] = sum([grid[0][k] for k in range(0, j)])
+                if j == 0:
+                    dp[i][j] = sum([grid[k][0] for k in range(0, i)])
+                else:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+        return dp[m - 1][n - 1]
+
+
+if __name__ == "__main__":
+    s = Solution()
+
+    result = s.minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]])
+
+    print(result)
