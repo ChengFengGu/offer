e1fc83ca8f8edbe2a188910538ea409155a298b8
GardenLu <lhypersonmail@163.com>
2021-10-21 16:27:01 +0800

    automatic push @2021年 10月 21日 星期四 16:27:01 CST

--- 动态规划/最小路径之和.py
+++ 动态规划/最小路径之和.py
@@ -5,16 +5,15 @@ class Solution:
 
         dp = [[0 for _ in range(n)] for _ in range(m)]
         dp[0][0] = grid[0][0]
+
         for i in range(m):
-            for j in range(n):
-                if i == 0:
-                    dp[0][j] = sum([grid[0][k] for k in range(0, j)]) + grid[i][j]
+            dp[i][0] = sum([grid[k][0] for k in range(0, i)]) + grid[i][0]
+        for j in range(n):
+            dp[0][j] = sum([grid[0][k] for k in range(0, j)]) + grid[0][j]
 
-                if j == 0:
-                    dp[i][0] = sum([grid[k][0] for k in range(0, i)]) + grid[i][j]
-
-                else:
-                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
         return dp[m - 1][n - 1]
 
 
