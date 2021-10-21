94fcf6dd81e93008bd2935e7d67769efeb8d99dc
GardenLu <lhypersonmail@163.com>
2021-10-21 15:24:01 +0800

    automatic push @2021年 10月 21日 星期四 15:24:01 CST

---
+++ 动态规划/子序列数目.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [[0 for _ in range(len(t))] for _ in range(len(s))]
+
+        # s为空串的时候
+        for j in range(len(t)):
+            if j == 0:
+                dp[0][j] = 1
+            else:
+                dp[0][j] = 0
+
+        # t为空串的时候
+        for i in range(len(s)):
+            dp[i][0] = 1
+
+        for j in range(1, len(t)):
+            for i in range(1, len(s)):
+                if s[i] != t[j]:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j] + 1
+        return dp[len(s) - 1][len(t) - 1]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.numDistinct("rabbbit", "rabbit")
+    print(result)
