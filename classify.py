a0b7b9638d756e12e6c1d9d320b8e9ccf42cc587
GardenLu <lhypersonmail@163.com>
2021-10-22 17:33:00 +0800

    automatic push @Fri Oct 22 17:33:00 CST 2021

--- classify.py
+++ classify.py
@@ -5,4 +5,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Model(nn.Module):
-    def __init__()
+    def __init__(self,):
+        self.conv = nn.Conv2d(28,56,kernel_size=3)
+        
+        self.layers = nn.Sequential(
+            nn.Conv2D(28,256,3)
+            nn.ReLu()
+            nn.Conv2d(256,512,3)
+            nn.ReLu()
+            nn.MaxPool()
+            nn.Conv2D()
+            
+        )
