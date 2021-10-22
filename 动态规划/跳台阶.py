0e1424aec132f9f23503668bb92ce248e61f6aa9
GardenLu <lhypersonmail@163.com>
2021-10-22 10:12:01 +0800

    automatic push @2021年 10月 22日 星期五 10:12:01 CST

--- 动态规划/跳台阶.py
+++ 动态规划/跳台阶.py
@@ -50,17 +50,17 @@ class Solutionv2(object):
             return 0
         if number == 1 or number == 2:
             return number
-        dp = [None for i in range(number)]
+        dp = [None for i in range(3)]
         dp[0] = 0
         dp[1] = 1
         dp[2] = 2
 
-        for i in range(3, number):
+        for i in range(3, number+1):
             if i < 3:
                 dp[i] = i
             else:
-                dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[number]
+                dp[i%3] = dp[(i - 1)%3] + dp[(i - 2)%3]
+        return dp[number%3]
 
 if __name__ == "__main__":
     sol = Solutionv2()
