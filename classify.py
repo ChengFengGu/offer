fc6fe0e8492642a2ce06bd9154387b0f8960988c
GardenLu <lhypersonmail@163.com>
2021-10-22 17:36:00 +0800

    automatic push @Fri Oct 22 17:36:00 CST 2021

--- classify.py
+++ classify.py
@@ -5,15 +5,27 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Model(nn.Module):
-    def __init__(self,):
-        self.conv = nn.Conv2d(28,56,kernel_size=3)
+    def __init__(self,in_feats:int):
+        # self.conv = nn.Conv2d(28,56,kernel_size=3)
         
         self.layers = nn.Sequential(
-            nn.Conv2D(28,256,3)
+            nn.Conv2D(in_feats,256,3)
             nn.ReLu()
             nn.Conv2d(256,512,3)
-            nn.ReLu()
+            nn.ReLU()
             nn.MaxPool()
-            nn.Conv2D()
+            nn.Conv2D(512,512,3)
+            nn.ReLU()
+            nn.AvgPool()
             
         )
+
+        self.cls = nn.Sequential(
+            nn.Linear(512,256)
+            nn.Linear(256,128)
+            nn.softmax(n=)
+        )
+
+    def forward(self,x):
+        x_feat = self.layers(x)
+        
