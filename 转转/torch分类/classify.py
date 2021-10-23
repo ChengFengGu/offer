55e009fab5634a1027a554c3d4094970a037ce42
GardenLu <lhypersonmail@163.com>
2021-10-23 10:09:01 +0800

    automatic push @Sat Oct 23 10:09:01 CST 2021

--- 转转/torch分类/classify.py
+++ 转转/torch分类/classify.py
@@ -4,8 +4,61 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision import transforms
 from torchvision.datasets import FashionMNIST
-
 import numpy as np
+import logging
+import os
+from os.path import join
+import traceback
+import sys
+
+
+def setup_logging(
+    output_folder, console="debug", info_filename="info.log", debug_filename="debug.log"
+):
+    """Set up logging files and console output.
+    Creates one file for INFO logs and one for DEBUG logs.
+    Args:
+        output_folder (str): creates the folder where to save the files.
+        debug (str):
+            if == "debug" prints on console debug messages and higher
+            if == "info"  prints on console info messages and higher
+            if == None does not use console (useful when a logger has already been set)
+        info_filename (str): the name of the info file. if None, don't create info file
+        debug_filename (str): the name of the debug file. if None, don't create debug file
+    """
+    if os.path.exists(output_folder):
+        raise FileExistsError(f"{output_folder} already exists!")
+    os.makedirs(output_folder, exist_ok=True)
+    # logging.Logger.manager.loggerDict.keys() to check which loggers are in use
+    base_formatter = logging.Formatter("%(asctime)s   %(message)s", "%Y-%m-%d %H:%M:%S")
+    logger = logging.getLogger("")
+    logger.setLevel(logging.DEBUG)
+
+    if info_filename != None:
+        info_file_handler = logging.FileHandler(join(output_folder, info_filename))
+        info_file_handler.setLevel(logging.INFO)
+        info_file_handler.setFormatter(base_formatter)
+        logger.addHandler(info_file_handler)
+
+    if debug_filename != None:
+        debug_file_handler = logging.FileHandler(join(output_folder, debug_filename))
+        debug_file_handler.setLevel(logging.DEBUG)
+        debug_file_handler.setFormatter(base_formatter)
+        logger.addHandler(debug_file_handler)
+
+    if console != None:
+        console_handler = logging.StreamHandler()
+        if console == "debug":
+            console_handler.setLevel(logging.DEBUG)
+        if console == "info":
+            console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(base_formatter)
+        logger.addHandler(console_handler)
+
+    def exception_handler(type_, value, tb):
+        logger.info("\n" + "".join(traceback.format_exception(type, value, tb)))
+
+    sys.excepthook = exception_handler
 
 
 def input_transform():
@@ -68,13 +121,14 @@ def get_set_loader():
     )
 
     train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    val_dataloader = DataLoader(val_dataset,batch_size=32,shuffle=False)
+    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
     test_dataloader = DataLoader(fashion_mnist_test, batch_size=32, shuffle=False)
 
-    return train_dataloader,val_dataloader,test_dataloader
+    return train_dataloader, val_dataloader, test_dataloader
+
 
 def train():
-    pass
+    train_loader,val_loader
 
 
 if __name__ == "__main__":
