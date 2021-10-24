296047142eef5955a7241e7ba4ed222d17ea8488
GardenLu <lhypersonmail@163.com>
2021-10-24 17:09:00 +0800

    automatic push @Sun Oct 24 17:09:00 CST 2021

---
+++ 字符串/查找共用字符串.py
@@ -0,0 +1,25 @@
+class Solution:
+    def char2index(self,char:str):
+        return ord(char)-ord('a')
+
+    def commonChars(self, words: List[str]) -> List[str]:
+        result = []
+        minfreq = [0 for _ in range(26)]
+        n = len(words)
+        for char in words[0]:
+            minfreq[self.char2index(char)] += 1
+        for i in range(1,n):
+            freq = [0 for _ in range(26)]
+            for char in words[i]:
+                freq[self.char2index(char)] += 1
+            for j in range(26):
+                minfreq[j] = min(minfreq[j],freq[j])
+        if i in range(26):
+            for j in range(0,minfreq[i]):
+                result.append(chr(i+ord('a')))
+        return result
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.commonChars(["bella","label","roller"])
+    
