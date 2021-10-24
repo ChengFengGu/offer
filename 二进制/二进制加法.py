f0f4ea127fba30f4a1cadc3ec492a9fcea1f6d04
GardenLu <lhypersonmail@163.com>
2021-10-24 13:12:00 +0800

    automatic push @Sun Oct 24 13:12:00 CST 2021

---
+++ 二进制/二进制加法.py
@@ -0,0 +1,26 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        result = []
+        str_result = ''
+        i = len(a)-1
+        j = len(b)-1
+
+        carry = 0
+        while (i>=0 or j >= 0):
+            digitA = int(a[i]) if i>=0 else 0
+            digitB = int(b[j]) if j>=0 else 0
+            i -= 1
+            j -= 1
+            sum = digitA + digitB + carry
+            carry = sum // 2
+            result.append(sum%2)
+        if carry == 1:
+            result.append(1)
+        for i in range(len(result)):
+            str_result += str(result[i])
+        return str_result
+
+if __name__ == "__main__":
+    s = Solution()
+    result =  s.addBinary("1","1")
+    print(result)
