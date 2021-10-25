801d81b0b38ff713a07649f9fcf81dc2f9e98504
GardenLu <lhypersonmail@163.com>
2021-10-25 20:45:01 +0800

    automatic push @2021年 10月 25日 星期一 20:45:01 CST

--- 因子度.py
+++ 因子度.py
@@ -8,13 +8,14 @@ if __name__ == '__main__':
     nums = [int(e) for e in line.split()]
 
     dp = [0 for k in range(0,max(nums)+1)]
-    dp[0] = None
-    dp[1] = None
+    dp[0] = -1
+    dp[1] = -1
     
     n = len(nums)
-    for k in range(2,max(nums)):
+    for k in range(2,max(nums)+1):
         for i in range(n):
             if nums[i] % k == 0:
-                dp[k]
+                dp[k] += 1
+    print(dp.index(max(dp)))
 
     
