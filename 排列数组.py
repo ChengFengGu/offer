16a0817bf7f1a047cf75c56532ea2a4b79c0a5fb
GardenLu <lhypersonmail@163.com>
2021-10-25 21:06:01 +0800

    automatic push @2021年 10月 25日 星期一 21:06:01 CST

--- 排列数组.py
+++ 排列数组.py
@@ -2,8 +2,12 @@
 if __name__ == '__main__':
     line = input()
     T = int(line)
+
+    s_data = []
+    t_data = []
     for i in range(T):
         line = input()
         k,n = tuple([int(e) for e in line.split()])
+        
         for j in range(n):
             
