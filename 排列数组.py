67a336fc93d657f5102368481c940912667c54a1
GardenLu <lhypersonmail@163.com>
2021-10-25 21:09:01 +0800

    automatic push @2021年 10月 25日 星期一 21:09:01 CST

--- 排列数组.py
+++ 排列数组.py
@@ -1,13 +1,20 @@
+def judge(s:str,t:str,swaps:list):
+    
+
 
 if __name__ == '__main__':
     line = input()
     T = int(line)
 
-    s_data = []
-    t_data = []
+    
     for i in range(T):
         line = input()
         k,n = tuple([int(e) for e in line.split()])
         
+        swaps = []
         for j in range(n):
-            
+            swaps.append(input())
+        s = input()
+        t = input()
+
+        
