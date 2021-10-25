42f0a461cb5d8b2f7dd69a12b90b6e3ddc84842a
GardenLu <lhypersonmail@163.com>
2021-10-25 20:51:01 +0800

    automatic push @2021年 10月 25日 星期一 20:51:01 CST

---
+++ 超级数.py
@@ -0,0 +1,10 @@
+
+if __name__ == '__main__':
+    line = input()
+    n,k = tuple([int(e) for e in line.split()])
+
+    line = input()
+    nums = [int(e) for e in line.split()]
+
+    while len(nums) > k:
+        
