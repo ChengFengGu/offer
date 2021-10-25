53ee1bb91df1e1f3ad70e5de678637caf95d25e3
GardenLu <lhypersonmail@163.com>
2021-10-25 21:18:01 +0800

    automatic push @2021年 10月 25日 星期一 21:18:01 CST

--- 排列数组.py
+++ 排列数组.py
@@ -1,23 +1,25 @@
-def judge(s:str,t:str,swaps:list,k:int):
-    for i in range(len(s)-1):
-        if s[i] != t[i] and s[i+1]!=t[i+1]:
-            if t[i]+t[i+1] in  or t[i+1]+t[i]
-            
+def judge(s: str, t: str, swaps: list, k: int):
+    for i in range(len(s) - 1):
+        if s[i] != t[i] and s[i + 1] != t[i + 1]:
+            if t[i] + t[i + 1] in swaps or t[i + 1] + t[i] in swaps:
+                temp = t[i]
+                t[i] = t[i + 1]
+                t[i + 1] = t[i]
+    
+    return True if s == t else False
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     line = input()
     T = int(line)
 
-    
     for i in range(T):
         line = input()
-        k,n = tuple([int(e) for e in line.split()])
-        
+        k, n = tuple([int(e) for e in line.split()])
+
         swaps = []
         for j in range(n):
             swaps.append(input())
         s = input()
         t = input()
-
-        
+        print(judge(s,t,swaps))
