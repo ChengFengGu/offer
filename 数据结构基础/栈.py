339df3f7936c9add6517fa6ea5f227888df7a695
GardenLu <lhypersonmail@163.com>
2021-10-26 10:00:01 +0800

    automatic push @2021年 10月 26日 星期二 10:00:01 CST

--- 数据结构基础/栈.py
+++ 数据结构基础/栈.py
@@ -1,8 +1,15 @@
 class Stack:
     def __init__(self):
         self.stack = []
-    def push(self,e):
+
+    def push(self, e):
         self.stack.append(e)
+
     def pop(self):
         self.stack.pop()
-    def get
+
+    def get(self):
+        return self.stack
+
+    def set(self, stack: list):
+        self.stack = stack
