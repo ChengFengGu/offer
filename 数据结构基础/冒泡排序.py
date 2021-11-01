0d66fdad523a7eb430e1736e9ab9cb60749ace4f
GardenLu <lhypersonmail@163.com>
2021-11-01 10:42:01 +0800

    automatic push @2021年 11月 01日 星期一 10:42:01 CST

--- 数据结构基础/冒泡排序.py
+++ 数据结构基础/冒泡排序.py
@@ -1,9 +1,12 @@
+class Student:
+    def __init__(self,val:int,name:str)
+
 class BubbleSort:
     def sort(self,array:list):
         n = len(array)
-        for i in range(n):
-            for j in range(n-i):
-                if array[j]<array[j+1]:
+        for i in range(n-1):
+            for j in range(n-i-1):
+                if array[j]>array[j+1]:
                     temp = array[j]
                     array[j] = array[j+1]
                     array[j+1] = temp
@@ -11,5 +14,5 @@ class BubbleSort:
 
 if __name__ == '__main__':
     bs = BubbleSort()
-    result = bs.sort([1,3,2,1])
+    result = bs.sort([1,3,2,1,5])
     print(result)
