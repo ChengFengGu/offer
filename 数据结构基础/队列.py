232e5018afc2c166899fb74e1b57b821afd623ca
GardenLu <lhypersonmail@163.com>
2021-11-01 11:24:01 +0800

    automatic push @2021年 11月 01日 星期一 11:24:01 CST

--- 数据结构基础/队列.py
+++ 数据结构基础/队列.py
@@ -1,4 +1,8 @@
 from queue import Queue
 
 class QueuseTest:
-    def dec
+    def decrypt(self,arr:list):
+        queue = Queue()
+        
+        def helper(array:list,left:int,right:int,result:Queue):
+            pass
