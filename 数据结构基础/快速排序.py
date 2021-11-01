7a35e86613a79a42a53c15502b6d6913dbed0508
GardenLu <lhypersonmail@163.com>
2021-11-01 10:48:01 +0800

    automatic push @2021年 11月 01日 星期一 10:48:01 CST

--- 数据结构基础/快速排序.py
+++ 数据结构基础/快速排序.py
@@ -1,3 +1,13 @@
 class QuickSort:
     def sort(self,arr:list):
-        
+        n = len(arr)
+        i = 0
+        j = n-1
+
+        base = arr[0]
+
+        while i<j:
+            while arr[j] >= base:
+                j -= 1
+            while arr[i] <= base:
+                i += 1
