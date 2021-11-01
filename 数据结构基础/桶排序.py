701d2735ef27b2b9c2a0c11decff5b83b53939e7
GardenLu <lhypersonmail@163.com>
2021-11-01 10:12:01 +0800

    automatic push @2021年 11月 01日 星期一 10:12:01 CST

--- 数据结构基础/桶排序.py
+++ 数据结构基础/桶排序.py
@@ -2,4 +2,11 @@
 
 class BucketSort:
     def sort(self,array:list):
-        pass
+        n = len(array)
+        bucket = [0 for _ in range(max(array)+1)]
+        for i in range(n):
+            bucket[array[i]] += 1
+        result = []
+        for i in range(len(bucket)):
+            for j in range(bucket[i]):
+                result.append(i)
