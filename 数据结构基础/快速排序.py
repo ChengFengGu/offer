e63e4e012274aee8909e8771a499386b46d990a8
GardenLu <lhypersonmail@163.com>
2021-11-01 11:09:01 +0800

    automatic push @2021年 11月 01日 星期一 11:09:01 CST

--- 数据结构基础/快速排序.py
+++ 数据结构基础/快速排序.py
@@ -1,22 +1,22 @@
 class QuickSort:
     def sort(self, arr: list):
         n = len(arr)
-        i = 0
-        j = n - 1
+        l = 0
+        r = n - 1
 
         def quick_sort(array: list, left: int, right: int):
-            base = array[left]
+            
             if left > right:
                 return array
-
+            base = array[left]
             i = left
             j = right
             base = array[left]
 
             while i < j:
-                while array[j] >= base:
+                while i < j and array[j] >= base:
                     j -= 1
-                while array[i] <= base:
+                while i < j and array[i] <= base:
                     i += 1
                 temp = array[i]
                 array[i] = array[j]
@@ -28,5 +28,12 @@ class QuickSort:
             array = quick_sort(array, left, i - 1)
             array = quick_sort(array, i + 1, right)
             return array
-            
+
+        return quick_sort(arr, l, r)
+
+
+if __name__ == "__main__":
+    qs = QuickSort()
+    result = qs.sort([1, 4, 3, 2, 5])
+    print(result)
 
