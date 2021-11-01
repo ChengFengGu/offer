3daca20b4d06cb06dece5287c4d9508454a67948
GardenLu <lhypersonmail@163.com>
2021-11-01 11:33:01 +0800

    automatic push @2021年 11月 01日 星期一 11:33:01 CST

--- 数据结构基础/队列.py
+++ 数据结构基础/队列.py
@@ -1,21 +1,23 @@
 from queue import Queue
 
+
 class QueuseTest:
-    def decrypt(self,arr:list):
+    def decrypt(self, arr: list):
         queue = Queue()
         n = len(arr)
         for i in range(n):
             queue.put(arr[i])
-        result = Queue()
-        while len(queue)>0:
-            if len(queue)>1:
-                result.put(queue.get())
+        result = []
+        while queue.qsize() > 0:
+            if queue.qsize() > 1:
+                result.append(queue.get())
                 queue.put(queue.get())
-            elif len(queue) == 1:
-                result.put(queue.get())
-        return list(result)
-    
-    
-if __name__ == '__main__':
+            elif queue.qsize() == 1:
+                result.append(queue.get())
+            
+
+
+if __name__ == "__main__":
     qt = QueuseTest()
-    result = qt.decrypt([])
+    result = qt.decrypt(["6", "3", "1", "7", "5", "8", "9", "2", "4"])
+    print(result)
