781af7ac1566478fe4580954ba48839c2b5ef1c3
GardenLu <lhypersonmail@163.com>
2021-11-01 10:54:01 +0800

    automatic push @2021年 11月 01日 星期一 10:54:01 CST

--- 数据结构基础/快速排序.py
+++ 数据结构基础/快速排序.py
@@ -4,14 +4,14 @@ class QuickSort:
         i = 0
         j = n-1
 
-        for b in range(n):
-            base = arr[b]
-            while i<j:
-                while i<j and arr[j] >= base:
-                    j -= 1
-                while i<j and arr[i] <= base:
-                    i += 1
-                temp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = temp
-            
+        def quick_sort(array:list,left:int,right:int):
+            base = array[left]
+            while left < right:
+                while array[right] >= base:
+                    right -= 1
+                while array[left] <= base:
+                    left += 1
+                temp = array[left]
+                array[left] = array[right]
+                array[right] = temp
+            return array
