c1f269282469a92b3cb9d1953aa167aba8f1cf4b
GardenLu <lhypersonmail@163.com>
2021-12-10 16:45:00 +0800

    automatic push @Fri Dec 10 16:45:00 CST 2021

--- 数据结构基础/堆化.py
+++ 数据结构基础/堆化.py
@@ -4,10 +4,10 @@ def left_child(index: int):
 def right_child(index: int):
     return index * 2 + 2
 
-
 def parent(index:int):
     if index == 0:
-        raise V
+        raise ValueError("根节点没有父节点")
+    return (index-1)//2
 
 
 def max_heap(arr: list):
