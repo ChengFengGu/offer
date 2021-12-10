34cf8def5561464c64c17d8c8b883045c7b54c85
GardenLu <lhypersonmail@163.com>
2021-12-10 16:42:00 +0800

    automatic push @Fri Dec 10 16:42:00 CST 2021

--- 数据结构基础/堆化.py
+++ 数据结构基础/堆化.py
@@ -1,3 +1,14 @@
+def left_child(index: int):
+    return index * 2 + 1
 
-def max_heap(arr:list):
-    
+def right_child(index: int):
+    return index * 2 + 2
+
+
+def parent(index:int):
+    if index == 0:
+        raise V
+
+
+def max_heap(arr: list):
+    pass
