3d115b4e5d741cbc536e01be557c39118a648bf3
GardenLu <lhypersonmail@163.com>
2022-06-05 12:39:01 +0800

    automatic push @2022年 06月 05日 星期日 12:39:01 CST

--- 推荐算法/基于物品的协同过滤.py
+++ 推荐算法/基于物品的协同过滤.py
@@ -1,4 +1,9 @@
 import numpy as np
+import pandas as pd
 
 items = [[3,4,3,1],[1,3,3,5],[2,4,1,5],[3,3,5,2],[3,5,4,1]]
-cols = [f'item{i}' for i in range(6)]
+cols = [f'item{i}' for i in range(1,6)]
+
+
+
+print(pd.DataFrame(np.corrcoef(items),columns=cols,index=cols))
