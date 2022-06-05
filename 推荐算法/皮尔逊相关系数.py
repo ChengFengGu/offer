a9696660b06893c7c20aa4cb74b63dd0d69ea5d5
GardenLu <lhypersonmail@163.com>
2022-06-05 12:00:01 +0800

    automatic push @2022年 06月 05日 星期日 12:00:01 CST

--- 推荐算法/皮尔逊相关系数.py
+++ 推荐算法/皮尔逊相关系数.py
@@ -1,8 +1,14 @@
 from scipy.stats import pearsonr
+from sklearn.metrics.pairwise import cosine_similarity
 
 A = [5,3,4,4]
 u1 = [3,1,2,3]
-u2 = 
-j = [1,0.5,0.5,0]
+u2 = [4,3,4,3]
+u3 = [3,3,1,5]
+u4 = [1,5,5,2]
 
-print(pearsonr(i,j))
+
+print(pearsonr(A,u1))
+print(pearsonr(A,u2))
+print(pearsonr(A,u3))
+print(pearsonr(A,u4))
